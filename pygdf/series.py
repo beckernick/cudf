a854cb38ec4b572b28e04ec4e5f12602433b8343
Nick Becker <nickb500@gmail.com>
2018-08-22 22:01:07 -0400

    expand to all non commutative reflected ops

--- pygdf/series.py
+++ pygdf/series.py
@@ -288,47 +288,33 @@ class Series(object):
         return self._binaryop(other, 'sub')
 
     def __rsub__(self, other):
-        # if isinstance(other, (int, float,
-        #                       np.int32, np.int64,
-        #                       np.float32, np.float64)):
-        #     empty = np.empty(len(self))
-        #     empty.fill(other)
-        #     other = Series(empty)
         other = self._normalize_binop_value(other)
         outcol = other._column.binary_operator('sub', self._column)
         return self._copy_construct(data=outcol)
-
-
 
     def __mul__(self, other):
         return self._binaryop(other, 'mul')
 
     def __rmul__(self, other):
-        return self.__mul__(other)
+        other = self._normalize_binop_value(other)
+        outcol = other._column.binary_operator('mul', self._column)
+        return self._copy_construct(data=outcol)
 
     def __floordiv__(self, other):
         return self._binaryop(other, 'floordiv')
 
     def __rfloordiv__(self, other):
-        if isinstance(other, (int, float,
-                              np.int32, np.int64,
-                              np.float32, np.float64)):
-            empty = np.empty(len(self))
-            empty.fill(other)
-            other = Series(empty)
-        return other.__floordiv__(self)
+        other = self._normalize_binop_value(other)
+        outcol = other._column.binary_operator('floordiv', self._column)
+        return self._copy_construct(data=outcol)
 
     def __truediv__(self, other):
         return self._binaryop(other, 'truediv')
 
     def __rtruediv__(self, other):
-        if isinstance(other, (int, float,
-                              np.int32, np.int64,
-                              np.float32, np.float64)):
-            empty = np.empty(len(self))
-            empty.fill(other)
-            other = Series(empty)
-        return other.__truediv__(self)
+        other = self._normalize_binop_value(other)
+        outcol = other._column.binary_operator('truediv', self._column)
+        return self._copy_construct(data=outcol)
 
     __div__ = __truediv__
 
